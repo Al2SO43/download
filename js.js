40af1ac7b9057107273cd66dc4d3f06a74c233e9
Al2SO43 <158013343+Al2SO43@users.noreply.github.com>
2024-05-01 17:50:17 +0800

    Add files via upload

--- js.js
+++ js.js
@@ -1,78 +1,32 @@
-function fetchAndRenderData() {
-    fetch('https://api.vvhan.com/api/hotlist/all')
-      .then(response => response.json())
-      .then(data => {
-        if (data.success) {
-          const hotlist = document.getElementById('hotlist');
-          hotlist.innerHTML = '';  // 清空原有内容
-  
-          data.data.forEach(item => {
-            const container = document.createElement('div');
-            container.classList.add('hotlist-container');
-            container.innerHTML = `
-              <h3>${item.name} - ${item.subtitle}</h3>
-              <p>更新时间: ${item.update_time}</p>
-              <ul>
-                ${item.data.map(entry => `
-                  <li><a href="${entry.url}" target="_blank">${entry.title}</a> <br>本条热度(或更新时间)：${entry.hot} </li>
-                `).join('')}
-              </ul>
-            `;
-            hotlist.appendChild(container);
-          });
-        } else {
-          console.error('API调用失败');
-        }
-      })
-      .catch(error => {
-        console.error('ERROR:404', error);
-      });
-  }
-
-  // 初次加载页面时执行并启动定时器，每半小时更新一次热搜数据
-  fetchAndRenderData();  // 确保页面首次加载时就获取并显示热搜数据
-  setInterval(fetchAndRenderData, 30 * 60 * 1000);  // 30分钟间隔
-  
-
+// 定义背景图片数组
+var backgroundImages = [
+    './img/background1.jpg','./img/background2.jpg','./img/background3.jpg','./img/background4.jpg','./img/background5.jpg','./img/background6.jpg','./img/background7.jpg','./img/background8.jpg','./img/background9.jpg','./img/background10.jpg'
+];
 // 随机选择背景图片
-const images = [
-    './bg/bg1.jpg',
-    './bg/bg2.jpg',
-    './bg/bg3.jpg',
-    './bg/bg4.jpg',
-    './bg/bg5.jpg',
-    './bg/bg6.jpg',
-    './bg/bg7.jpg',
-    './bg/bg8.jpg',
-    './bg/bg9.jpg',
-    './bg/bg10.jpg',
-  ];
-  const randomIndex = Math.floor(Math.random() * images.length);
-  document.body.style.backgroundImage = `url(${images[randomIndex]})`;
-
-
-  function displayTime() {
-    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false, timeZone: 'Asia/Shanghai' };
-    const beijingTime = new Date().toLocaleString('zh-CN', options);
-    document.getElementById('datetime').textContent = beijingTime;
-  }
-
-  // 每秒更新时间
-  setInterval(displayTime, 1000);
-
-  // 初次加载页面时显示时间
-  displayTime();
-
-
+var randomIndex = Math.floor(Math.random() * backgroundImages.length);
+var selectedImage = backgroundImages[randomIndex];
+// 设置背景图片
+document.body.style.backgroundImage = 'url(' + selectedImage + ')';
 // 显示弹窗
 window.onload = function() {
   var modal = document.createElement('div');
   modal.classList.add('modal');
-  modal.innerHTML = '<p>温馨提示:本站热榜内容不代表作者本人最终观点,请理性对待这些热榜信息!</p>';
+  modal.innerHTML = '<p>请在阅读<a href="https://wwt.lanzouj.com/b00l0qk7cd">『硫酸铝的下载站』相关文档</a>之后浏览本站内容,否则因各种原因发生的任何问题的后果将全部由您承担!</p>';
   document.body.appendChild(modal);
   modal.style.display = 'block';
   setTimeout(function() {
     modal.style.display = 'none';
-  }, 3500); // 3.5秒后自动隐藏
+  }, 7000); // 7秒后自动隐藏
 };
 
+function displayTime() {
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false, timeZone: 'Asia/Shanghai' };
+  const beijingTime = new Date().toLocaleString('zh-CN', options);
+  document.getElementById('datetime').textContent = beijingTime;
+}
+
+// 每秒更新时间
+setInterval(displayTime, 1000);
+
+// 初次加载页面时显示时间
+displayTime();
