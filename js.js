cac7513d0089f5bd1c0702412f5b6dc204d7f772
Al2SO43 <158013343+Al2SO43@users.noreply.github.com>
2024-05-02 20:50:09 +0800

    Add files via upload

--- js.js
+++ js.js
@@ -11,7 +11,8 @@ document.body.style.backgroundImage = 'url(' + selectedImage + ')';
 window.onload = function() {
   var modal = document.createElement('div');
   modal.classList.add('modal');
-  modal.innerHTML = '<p>请在阅读 <a href="https://wwt.lanzouj.com/b00l0qk7cd">『硫酸铝的下载站』相关文档</a> 之后浏览本站内容,否则因各种原因发生的任何问题的后果将全部由您承担!</p>';
+  var encodedLink = encodeURI('『硫酸铝的下载站』相关文档.pdf');
+  modal.innerHTML = '<p>请在阅读 <a href="' + encodedLink + '">『硫酸铝的下载站』相关文档</a> 之后浏览本站内容,否则因各种原因发生的任何问题的后果将全部由您承担!</p>';
   document.body.appendChild(modal);
   modal.style.display = 'block';
   setTimeout(function() {
@@ -30,3 +31,12 @@ setInterval(displayTime, 1000);
 
 // 初次加载页面时显示时间
 displayTime();
+
+document.addEventListener('DOMContentLoaded', function() {
+  var buttons = document.querySelectorAll('button a');
+  buttons.forEach(function(button) {
+    var href = button.getAttribute('href');
+    var encodedHref = encodeURI(href);
+    button.setAttribute('href', encodedHref);
+  });
+});
