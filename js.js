b8d46b52b2fc4fe169a3945fbe7dc23f1bdf9c6a
Al2SO43 <158013343+Al2SO43@users.noreply.github.com>
2024-06-24 15:17:42 +0800

    Add files via upload

--- js.js
+++ js.js
@@ -7,17 +7,6 @@ var randomIndex = Math.floor(Math.random() * backgroundImages.length);
 var selectedImage = backgroundImages[randomIndex];
 // 设置背景图片
 document.body.style.backgroundImage = 'url(' + selectedImage + ')';
-// 显示弹窗
-window.onload = function() {
-  var modal = document.createElement('div');
-  modal.classList.add('modal');
-  modal.innerHTML = '<p>请在阅读<a href="DownloadDocument.pdf" target="_blank">『硫酸铝的下载站---相关文档』</a>之后浏览本站内容,否则因各种原因发生的任何问题的后果将全部由您承担!</p>';
-  document.body.appendChild(modal);
-  modal.style.display = 'block';
-  setTimeout(function() {
-    modal.style.display = 'none';
-  }, 7000); // 7秒后自动隐藏
-};
 
 function displayTime() {
   const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false, timeZone: 'Asia/Shanghai' };
